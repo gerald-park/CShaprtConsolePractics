9624c4441394ff32ef1024138d0ae6b4a3894459
Gerald Park <gerald.park@edwardsvacuum.com>
2023-01-08 06:47:57 +0900

    Start with console

---
+++ CShaprtConsolePractics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CShaprtConsolePractics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string message = Console.ReadLine ();
+            Console.WriteLine($"Echo : {message}");
+            Console.WriteLine(message);
+        }
+    }
+}
